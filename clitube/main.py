da90e405b5e43f945a9b1586d6f796dda80257bc
Void <stas050595@gmail.com>
2025-10-11 15:06:57 +0000

    Rewritten the project with typer library.

--- clitube/main.py
+++ clitube/main.py
@@ -1,79 +1,121 @@
-import os , argparse , subprocess , sys                     # Crucial libraries.
-from colorama import Fore , Style , init               
+import typer , subprocess , tomli
+from rich.console import Console
+from random import choice
 
-init() # initiating colorama.
+app = typer.Typer(name="clitube")
+console = Console()
 
-def check_args(args): # function which was made to determine if any arguments were passed.
-    global parser # get args
-    no_vars = True 
-    for var , value in vars(args).items(): # iterate over arguments
-        if value not in [False , "False" , None , "None"]: # treat both Python None and string "None"/"False" as "not provided"
-            no_vars = False # set "no argument" variable to false.
-        else: # skip if its False.
-            pass 
+downloadColors = [
+    "bold purple",
+    "bold magenta",
+    "bold cyan",
+]
 
-    if no_vars is True:
-        print(parser.description) # print script's description and exit.
-        sys.exit()
+playColors = [
+    "bold yellow",
+    "bold blue"
+]
 
-def main():
-    global parser # make parser global so check_args can access parser's description.
-    parser = argparse.ArgumentParser(description="Small script that allows you to control youtube through terminal!")
+logStatuses = {
+    "info": "bold white",
+    "success": "bold green",
+    "warn": "bold yellow",
+    "error": "bold red",
+    "fatal": "bold dark_red"
+}
 
-    parser.add_argument("-n" , "--name" , help="name of video to search it!")
-    parser.add_argument("-id" , help="ID of the video to use it instead of searching!")
-    parser.add_argument("-p" , "--play" , action="store_true" , help="play the video!")
-    parser.add_argument("-d" , "--download" , action="store_true" , help="download the video!")
-    parser.add_argument("-s" , "--search" , type=int , help="number of searches for the ID!")
-    parser.add_argument("-l" , "--loop" , action="store_true" , help="loop the video!")
-    parser.add_argument("-nv" , "--no-video" , action="store_true" , help="disable the video!")
-    parser.add_argument("-na" , "--no-audio" , action="store_true" , help="disable the audio!")
+class LogStatusError(BaseException):
+    pass
 
-    args = parser.parse_args() 
-
-    check_args(args) # check args
-
-    if (args.name and args.id) is (False or None or "False" or "None"): # check if name or id of video is passed.
-        return print("No name/id given! Unable to" + Style.BRIGHT + Fore.RED + " proceed." + Style.RESET_ALL)
-        
-    if (args.play and args.download and args.search) is (False or None or "False" or "None"): # check if any action is selected.
-        return print("No action selected!" + Style.BRIGHT + Fore.RED + " Quiting..." + Style.RESET_ALL)
-
-    if args.name and not args.search and not args.id: # get video's id
-        video_id = subprocess.run(f"yt-dlp 'ytsearch1: {args.name}' --get-id" , capture_output=True , shell=True , text=True).stdout.strip()
+def log(message: str , status: str):
+    if status not in logStatuses.keys():
+        raise LogStatusError("Unknown status.")
     
-        if video_id == '': # exit if no video/internet.
-            print(Style.BRIGHT , Fore.RED + "No Video Found!" , Style.RESET_ALL)
+    console.print(f"[{logStatuses[status.lower()]}][{status.upper()}][/{logStatuses[status.lower()]}] [bold white]{message}[/bold white]")
+
+@app.command()
+def play(name: str=None , id: str=None , loop: bool=False , noAudio: bool=False , noVideo: bool=False , debug: bool=False):
+    if debug:
+        log("Checking if passed arguments meet requirements..." , "info")
+    if id and name:
+        raise typer.BadParameter("You cant use both 'id' and 'name'!")
+    if not (id or name):
+        raise typer.BadParameter("You must pass either 'id' or 'name'!")
+    if debug:
+        log("All arguments pass requirements. Proceeding..." , "success")
+    
+    if debug:
+        log("Randomizing color..." , "info")
+    color = choice(playColors)
+    if debug:
+        log(f"Color is {color}" , "success")
+
+    if name:
+        console.print(f"[bold white]Fetching video [bold {color}]ID[/bold {color}] based on video name...[/bold white]")
+        video_id = subprocess.run(["yt-dlp" , f"ytsearch: {name}" , "--get-id"] , capture_output=True , text=True).stdout.strip()
+        if video_id == '':
+            if debug:
+                log("Empty video id. exiting..." , "fatal")
+            console.print(f"[bold red]No videos found! [/bold red][bold white]Consider adding [bold {color}]--debug[/bold {color}].[/bold white]")
             return
-            
-        command = f'mpv --ytdl-raw-options=\'extractor-args="youtube:player-client=default,-tv_simply"\' https://youtube.com/watch?v={video_id}' # make basic command.
-
-    if args.id:
-        command = f'mpv --ytdl-raw-options=\'extractor-args="youtube:player-client=default,-tv_simply"\' https://youtube.com/watch?v={args.id}'
- # skip getting id if id is already passed.
     
-    if args.play: # play action logic.
-        if args.loop:
-            command = f"{command} --loop"
-        if args.no_video:
-            command = f"{command} --no-video"
-        if args.no_audio:
-            command = f"{command} --no-audio"
+    args = []
+    
+    if noVideo:
+        args.append("--no-video")
+    if noAudio:
+        args.append("--no-audio")
+    if loop:
+        args.append("--loop")
+    
+    console.print(f"[bold white]Playing [bold {color}]{name or id}[/bold {color}][/bold white]")
+    subprocess.Popen(["mpv" , f"ytdl://https://youtube.com/watch?v={video_id}" , *args]).wait()
 
-        os.system(command)
-    elif args.download: # download action logic.
-        os.system(f"yt-dlp {command.removeprefix('mpv').strip()}")
+@app.command() 
+def download(name: str=None , id: str=None , debug: bool=False):
+    if debug:
+        log("Checking if passed arguments meet requirements..." , "info")
+    if id and name:
+        raise typer.BadParameter("You cant use both 'id' and 'name'!")
+    if not (id or name):
+        raise typer.BadParameter("You must pass either 'id' or 'name'!")
+    if debug:
+        log("All arguments pass requirements. Proceeding..." , "success")
+    
+    if debug:
+        log("Randomizing color..." , "info")
+    color = choice(downloadColors)
+    if debug:
+        log(f"Color is {color}" , "success")
+    console.print(f"[bold white]Downloading [{color}]{name or id}[/{color}]...[/bold white]")
 
-    if args.search: # search action logic.
-        if not args.name:
-            print("Please enter a --name/-name argument with name to search!") # if no name is passed skip. (POSSIBLE OPTIMIZATION HERE)
-            return 0
-        print(Style.BRIGHT , Fore.WHITE + "Searching..." , Style.RESET_ALL)
-        video_data = subprocess.run(["yt-dlp" , f"ytsearch{int(args.search)}: {args.name}" , "--get-id" , "--get-title"] , capture_output=True , text=True) # download using yt-dlp
-        if video_data.stdout != '': # if output is not an empty string, print.
-            print(video_data.stdout)
-        else: # if output is empty.
-            print(Style.BRIGHT , Fore.RED + "No Videos Found!" , Style.RESET_ALL) 
+    if name:
+        if debug:
+            log(f"Downloading video based on name..." , "info")
+        subprocess.run(["yt-dlp" , f"ytsearch: {name}"])
+        if debug:
+            log(f"Video downloaded." , "success")
+    else:
+        if debug:
+            log(f"Downloading video based on id..." , "info")
+        subprocess.run(["yt-dlp" , id])
+        if debug:
+            log("Video downloaded." , "success")
+
+@app.command()
+def version(debug: bool=False):
+    if debug:
+        log("Extracting pyproject.toml for version..." , "info")
+    version = tomli.load(open("pyproject.toml" , "rb"))["project"]["version"]
+    if debug:
+        log("Successfully extracted version." , "success")
+    console.print(f"[bold red]Cli[/bold red][bold white]Tube - {version}[/bold white]")
+
+@app.command()
+def search(amount: int , name: str , debug: bool=False):
+    console.print(f"[bold white]Searching {amount} videos with name similar to '{name}'...")
+
+    subprocess.run(["yt-dlp" , f"ytsearch{amount}: {name}" , "--get-id" , "--get-title"])
 
 if __name__ == "__main__":
-    main() # run
+    app()
